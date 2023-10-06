6191866c7fa6133f5bd4ab2a33a1a96ca9120142
MateenCPH <cph-mr645@cphbusiness.dk>
2023-10-06 10:48:14 +0200

    Task2 rettet efter review

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -7,10 +7,23 @@ public class Main {
     public static void main(String[] args) {
         Cafe cafeOne = new Cafe();
         cafeOne.loadMenuData();
-        //for (int i = 0; i < file.length; i++); {
-        //Kan simpelthen ikke finde ud af denne del... (opg 2.h)
+        displayMenu(cafeOne);
     }
 
+    /*public static void displayMenu(Cafe cafeOne) {
+        for (int i = 0; i < cafeOne.getCoffeeMenu().size(); i++) ;
+        {
+            int i = 0;
+            String coffee = cafeOne.getCoffeeMenu().get(i);
+            System.out.println(coffee);
+        }
+    }*/
+
+    public static void displayMenu(Cafe cafeOne) {
+        for (String menuItem : cafeOne.getCoffeeMenu()) {
+            System.out.println(menuItem);
+        }
+    }
 
 }
 
